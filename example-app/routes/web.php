055b5ebf38976516a0341f532ebd97fda676e0ed
NguyenLuHaiDuy <haiduynguyenlu@gmail.com>
2024-07-25 13:05:48 +0700

    fix lỗi chạy localhost

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,13 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\Product\CategoryController;
-
-use App\Http\Controllers\client\HomepageController;
 use App\Http\Controllers\Admin\AdminController;
-
-
-
+use App\Http\Controllers\client\HomepageController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 // client
 Route::get('/',[HomepageController::class , 'index'])->name('homepage');
 Route::get('/sanpham',[HomepageController::class , 'products'])->name('product');
@@ -15,10 +11,7 @@ Route::get('/baiviet',[HomepageController::class , 'blog'])->name('blog');
 Route::get('/lienhe',[HomepageController::class , 'contact'])->name('contact');
 Route::get('/khac',[HomepageController::class , 'Orther'])->name('orther');
 
-
-
 // admin
-
 Route::get('/admin',[AdminController::class , 'index'])->name('admin');
 Route::get('/productsAdm',[AdminController::class , 'productsAdm'])->name('productsAdm');
 Route::get('/dashboard',[AdminController::class , 'dashboard'])->name('dashboard');
@@ -30,3 +23,18 @@ Route::get('/orders',[AdminController::class , 'orders'])->name('orders');
 Route::get('/addProd',[AdminController::class , 'addProd'])->name('addProd');
 Route::get('/addCate',[AdminController::class , 'addCate'])->name('addCate');
 Route::get('/ordersDetail',[AdminController::class , 'ordersDetail'])->name('ordersDetail');
+
+
+
+// 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
